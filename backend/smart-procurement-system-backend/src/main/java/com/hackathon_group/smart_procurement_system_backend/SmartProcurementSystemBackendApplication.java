4c256c511f8a2a42869e79df2255f2d9de425ae4
Avinash Kumar <avinash773946@gmail.com>
2026-08-29 20:38:08 +0530

    Initial backend setup

---
+++ backend/smart-procurement-system-backend/src/main/java/com/hackathon_group/smart_procurement_system_backend/SmartProcurementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon_group.smart_procurement_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartProcurementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartProcurementSystemBackendApplication.class, args);
+	}
+
+}
---
+++ backend/smart-procurement-system-backend/src/test/java/com/hackathon_group/smart_procurement_system_backend/SmartProcurementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon_group.smart_procurement_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartProcurementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
